ac454ce10d4d8ccfa2bdbf31c39673dc9d120a11
Dmytro Savchuk <xsavchuk@fi.muni.cz>
2023-09-30 21:13:07 +0200

    release beta 0.3b Fixed button colors in Login and Register

--- src/components/pages/Register/Register.tsx
+++ src/components/pages/Register/Register.tsx
@@ -17,6 +17,18 @@ const Register = () => {
     const {t} = useTranslation();
     const navigate = useNavigate();
 
+    const firebaseConfig = {
+        apiKey: "AIzaSyB1z8SO5WAUdLe_1qu8nyOP5-A8xy4yyfM",
+        authDomain: "fantqtravel.firebaseapp.com",
+        projectId: "fantqtravel",
+        storageBucket: "fantqtravel.appspot.com",
+        messagingSenderId: "682251622555",
+        appId: "1:682251622555:web:eff3fc46bfe07275c38e3e",
+        measurementId: "G-Y1RX9J6CR1"
+    };
+     
+    firebase.initializeApp(firebaseConfig);
+
 
     useEffect(()=>{
         firebase.auth().onAuthStateChanged((user) =>{
@@ -71,13 +83,13 @@ const Register = () => {
                     <form onSubmit={(e)=>{signUpEmail(e)}}>
                         <input type="email" placeholder='E-mail' value={inputEmail} onChange={(e)=>{setInputEmail(e.target.value)}} required/>
                         <input type="password" placeholder={t('register.password')} value={inputPassword} onChange={(e)=>{setInputPassword(e.target.value)}} required/>
-                        <button>{t('register.sign_up')}</button>
+                        <button className={classes.button}>{t('register.sign_up')}</button>
                         {errorMessages && (
                             <p style={{color:'red'}}>{errorMessages}</p>
                         )}
                     </form>
                     <p>{t('register.or_sign_with')}</p>
-                    <button className={classes.signInGoogle} onClick={signUpGoogle}>
+                    <button className={`${classes.button} ${classes.signInGoogle}`} onClick={signUpGoogle}>
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 30 30" width="30px" height="30px">    <path d="M 15.003906 3 C 8.3749062 3 3 8.373 3 15 C 3 21.627 8.3749062 27 15.003906 27 C 25.013906 27 27.269078 17.707 26.330078 13 L 25 13 L 22.732422 13 L 15 13 L 15 17 L 22.738281 17 C 21.848702 20.448251 18.725955 23 15 23 C 10.582 23 7 19.418 7 15 C 7 10.582 10.582 7 15 7 C 17.009 7 18.839141 7.74575 20.244141 8.96875 L 23.085938 6.1289062 C 20.951937 4.1849063 18.116906 3 15.003906 3 z" /></svg>
                         Google
                     </button>
